f55fca03e588d39516cf90d6df51b0a7cdd276ea
Krylova Nataliya <natalinebo2014@yandex.ru>
2022-01-27 09:10:29 +0100

    complete task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,29 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    console.log(expr);
+	let arr = expr.match(/.{1,10}/g);  /*разбили строку в массив букв */
+	console.log(arr);
+	for(let i = 0; i < arr.length; i++) {
+		arr[i] = arr[i].replace(/11/g, "-");
+		arr[i] = arr[i].replace(/10/g, ".");
+		arr[i] = arr[i].replace(/00/g, "");
+		for (let key in MORSE_TABLE) {
+			if (arr[i] === key) {
+				arr[i] = MORSE_TABLE[key]
+			}
+		}
+	}
+	console.log(arr);
+	let finalStr = arr.join('');
+
+	 if (finalStr.includes('**********')) {
+		finalStr = finalStr.replaceAll('**********', ' ');
+	 }
+	 
+	console.log(finalStr);
+	 return finalStr;
+    
 }
 
 module.exports = {
